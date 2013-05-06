40102a980d3a585ab01b721071056aa312e11ba7
svanderbleek <sandy.vanderbleek@gmail.com>
2013-05-06 13:55:11 -0700

    cute name

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-PintProgrammer::Application.load_tasks
+Tun::Application.load_tasks
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
-PintProgrammer::Application.initialize!
+Tun::Application.initialize!
